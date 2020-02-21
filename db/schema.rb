a2c6200bc48f86870863cd86deb15e50399ab522
Turzhanskyi <turzhansky81@gmail.com>
2020-02-22 00:55:39 +0200

    Tags added to Posts

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_200_221_201_512) do
+ActiveRecord::Schema.define(version: 20_200_221_220_343) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -29,5 +29,20 @@ ActiveRecord::Schema.define(version: 20_200_221_201_512) do
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
     t.string   'image'
+  end
+
+  create_table 'taggings', force: :cascade do |t|
+    t.integer  'post_id'
+    t.integer  'tag_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['post_id'], name: 'index_taggings_on_post_id', using: :btree
+    t.index ['tag_id'], name: 'index_taggings_on_tag_id', using: :btree
+  end
+
+  create_table 'tags', force: :cascade do |t|
+    t.string   'name'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 end
