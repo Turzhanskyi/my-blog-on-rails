a3349a6476e22b04c469118244f509338ca42a0b
Turzhanskyi <turzhansky81@gmail.com>
2020-02-22 22:29:26 +0200

    Sub-menu categories added to header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,11 +4,11 @@ module ApplicationHelper
   def build_breadcrumb(category)
     result = []
     category.ancestors.each do |ancestor|
-      result << content_tag(:li, class: 'breadcrumb-item') do
+      result << content_tag(:li, class: "breadcrumb-item") do
         link_to(ancestor.name, ancestor)
       end
     end
-    result << content_tag(:li, category.name, class: 'breadcrumb-item active')
-    result.join('').html_safe
+    result << content_tag(:li, category.name, class: "breadcrumb-item active")
+    result.join("").html_safe
   end
 end
