6e173e45780dd7a6059f8ff415e932bb4c50fdc6
Turzhanskyi <turzhansky81@gmail.com>
2020-02-22 21:21:05 +0200

    Breadcrumb added to categories

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def build_breadcrumb(category)
+    result = []
+    category.ancestors.each do |ancestor|
+      result << content_tag(:li, class: 'breadcrumb-item') do
+        link_to(ancestor.name, ancestor)
+      end
+    end
+    result << content_tag(:li, category.name, class: 'breadcrumb-item active')
+    result.join('').html_safe
+  end
 end
